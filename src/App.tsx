31789ffcfd08789e91f2ecdf9da9ab4af80dd3b4
ttrebicki <tomek.trebicki@goingapp.pl>
2024-01-02 16:14:26 +0100

    some fixes and text feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,35 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import "./App.css";
 import { getRandomColors } from "./classes/ColorGenerator";
+
+const renderImage = (context: CanvasRenderingContext2D | null, imageUrl: string, x:number, y:number, dx: number, dy: number) => {
+  if (context) {
+    // Create an image element
+    const img = new Image();
+
+    // Set the source of the image to the provided URL
+    img.src = imageUrl;
+
+
+    // When the image is loaded, draw it on the canvas
+    // img.onload = () => {
+    //     context.drawImage(img, x, y, dx, dy);
+    // };
+
+    const animate = () => {
+      // Draw the image at the current position
+      context.drawImage(img, x, y, dx,dy);
+
+      // Update the position for the next frame
+      x += 2; // Adjust the value based on your desired speed
+    }
+      // Request the next animation frame
+      requestAnimationFrame(animate);
+    
+} else {
+    console.error('Unable to get 2D context for canvas.');
+}
+}
 
 export interface IPosition {
   x1: number;
@@ -15,24 +44,25 @@ const compareNumbersWithRange = (
   range: number
 ) => num1 < num2 + range && num1 > num2 - range;
 
-const range = 0.1;
+const range = 0.128;
 
-const cellSpawner = async (
-  cellsNumber: number,
+const cellSpawner = async ({cellPositionsArray, cellsNumber, context, randomColors, dimensions}:
+  {cellsNumber: number,
   context: CanvasRenderingContext2D,
   cellPositionsArray: IPosition[],
-  randomColors?: { color1: string; color2: string; color3: string; }
+  randomColors?: { color1: string; color2: string; color3: string; },
+  dimensions?: [number,number]}
 ) => {
   for (let i = 0; i < cellsNumber; i++) {
     // await new Promise<void>((res) => setTimeout(() => {res()}, i * 1000))
 
-    const mainColor = "#fff";
+    const mainColor = "#ccc";
     const secondaryColor = "#000";
-    const ternaryColor = "#f0f";
+    const ternaryColor = "#f33";
     context.strokeStyle = mainColor;
 
-    const randomX = Math.ceil(Math.random() * 300);
-    const randomY = Math.ceil(Math.random() * 150);
+    const randomX = Math.ceil(Math.random() * (dimensions?.[0] || 0 / 5 || 300));
+    const randomY = Math.ceil(Math.random() * (dimensions?.[1] || 0 / 5 || 150));
     const lineFinishX = randomX % 2 === 0 ? randomX - 4 : randomX + 4;
     const lineFinishY = randomY + 4;
 
@@ -43,6 +73,10 @@ const cellSpawner = async (
     if (randomX % 3 === 0) {
       context.strokeStyle = ternaryColor;
     }
+
+    // if (randomX % 2 === 0 || randomY % 4 === 0) {
+    //   renderImage(context, 'https://www.pngall.com/wp-content/uploads/14/Thumbs-Up-Emoji-PNG-Photos.png', randomX*i, randomY*i, i,i)
+    // }
 
     const position = {
       x1: randomX,
@@ -70,7 +104,8 @@ const cellSpawner = async (
       grd.addColorStop(0.5, randomColors?.color2 || '#dcc');
       grd.addColorStop(1, randomColors?.color3 || '#fac');
       context.strokeStyle = grd;
-      context.arc(position.x1, position.y1, 0.1, 0, 2 * Math.PI);
+      context.strokeText('BEVERLY HILLS 90210', position.x1, position.y1)
+      // context.arc(position.x1, position.y1, 48, 320, 2 * Math.PI);
       context.stroke();
       return;
     }
@@ -85,26 +120,52 @@ const cellSpawner = async (
 
 function App() {
   const [gradientColors, setGradientColors] = useState<{ color1: string; color2: string; color3: string; }>()
-
-  useEffect(() => {
-      const cellPositionsArray: IPosition[] = [];
+  const [windowDimensions, setWindowDimensions] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+  const cb = useCallback(() => {
+    const cellPositionsArray: IPosition[] = [];
       const gameCanvas: HTMLCanvasElement = document.getElementById(
         "game"
       ) as HTMLCanvasElement;
       const context = gameCanvas?.getContext("2d");
-      const x = setInterval(
-        () => cellSpawner(48, context as CanvasRenderingContext2D, cellPositionsArray, gradientColors),
-        0.1
-      );
-  }, [gradientColors]);
+      cellSpawner({cellsNumber: 128000, context: context as CanvasRenderingContext2D, cellPositionsArray, randomColors: gradientColors, dimensions: [windowDimensions.width, windowDimensions.height]})
+  }, [gradientColors])
 
   useEffect(() => {
-    setInterval(() => setGradientColors(getRandomColors()), 15000)
+    cb()
+  }, [cb]);
+
+  useEffect(() => {
+    setInterval(() => setGradientColors(getRandomColors()), 0.00001)
+    
   }, [])
+
+  useEffect(() => {
+    // Function to update window dimensions
+    const updateWindowDimensions = () => {
+      setWindowDimensions({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+
+    // Event listener for window resize
+    window.addEventListener('resize', updateWindowDimensions);
+
+    // Call the function to set initial window dimensions
+    updateWindowDimensions();
+
+    // Cleanup: remove event listener on component unmount
+    return () => {
+      window.removeEventListener('resize', updateWindowDimensions);
+    };
+  }, []);
 
   return (
     <>
-      <canvas id={"game"} className={"game"} />
+      <canvas width={windowDimensions.width / 5} height={windowDimensions.height / 5} id={"game"} className={"game"} />
     </>
   );
 }
